418698c7dc8e588cd8b3a8f606e2e276a4dd43a4
Marc <marccr04@gmail.com>
2024-12-21 16:58:10 +0100

    Primera versió de vehicleID

--- src/data/VehicleID.java
+++ src/data/VehicleID.java
@@ -4,7 +4,10 @@ public class VehicleID {
     //formato del id: 4 números (como una matrícula pero sin letras)
     private int[] id;
 
-    public VehicleID (int[] receivedId){
-
+    public VehicleID (int[] receivedId) throws NotCorrectFormatException {
+        if (receivedId.length != 4){
+            throw new NotCorrectFormatException("El formato del id del vehículo es de 4 números");
+        }
+        this.id = receivedId;
     }
 }
